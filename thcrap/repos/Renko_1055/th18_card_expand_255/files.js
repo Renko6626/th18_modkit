b0cf2e6a0ea68035bdc4e04970921d82812e6859
Renko6626 <renko6626@gmail.com>
2026-09-04 01:35:42 +0800

    card-expand: 同步构建产物（renkolab afa6b12）
    DLL md5 99ab34dc…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/ability.anm
      mods/th18_card_expand.dll
      thcrap/repos/Renko_1055/th18_card_expand_255/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

--- thcrap/repos/Renko_1055/th18_card_expand_255/files.js
+++ thcrap/repos/Renko_1055/th18_card_expand_255/files.js
@@ -2,6 +2,6 @@
   "patch.js": 1388925521,
   "th18.v1.00a.js": 2190090729,
   "th18/abcard.anm": 2927454332,
-  "th18/ability.anm": 4118672995,
+  "th18/ability.anm": 3956597003,
   "th18/cards.js": 3294759511
 }
