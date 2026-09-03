62292b52110195074b3f851b9ad33a227c2310ac
Renko6626 <renko6626@gmail.com>
2026-09-04 00:55:24 +0800

    card-expand: 同步构建产物（renkolab 78ad448）
    DLL md5 95e3fadf…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/cards.js
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/abcard.anm
      thcrap/repos/Renko_1055/th18_card_expand_255/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

--- thcrap/repos/Renko_1055/th18_card_expand_255/th18/cards.js
+++ thcrap/repos/Renko_1055/th18_card_expand_255/th18/cards.js
@@ -3,31 +3,31 @@
     "name": "黑桃 10",
     "desc": ["从道具获得的金钱 +10％", "（每 10 个金钱道具多给 1）。", "德州扑克・皇家同花顺之一。"],
     "internal_name": "SPADE_10",
-    "initial_unlocked": 1, "price_tier": 5, "weight": 2, "sprite_large": 116, "sprite_small": 117
+    "initial_unlocked": 1, "price_tier": 5, "weight": 2, "sprite_large": 118, "sprite_small": 119
   },
   "59": {
     "name": "黑桃 J",
     "desc": ["移动速度 +10％。", "德州扑克・皇家同花顺之一。"],
     "internal_name": "SPADE_J",
-    "initial_unlocked": 1, "price_tier": 6, "weight": 2, "sprite_large": 116, "sprite_small": 117
+    "initial_unlocked": 1, "price_tier": 6, "weight": 2, "sprite_large": 120, "sprite_small": 121
   },
   "60": {
     "name": "黑桃 Q",
     "desc": ["道具自动回收范围大幅增加。", "德州扑克・皇家同花顺之一。"],
     "internal_name": "SPADE_Q",
-    "initial_unlocked": 1, "price_tier": 7, "weight": 2, "sprite_large": 116, "sprite_small": 117
+    "initial_unlocked": 1, "price_tier": 7, "weight": 2, "sprite_large": 122, "sprite_small": 123
   },
   "61": {
     "name": "黑桃 K",
     "desc": ["自机弹伤害 +10％。", "德州扑克・皇家同花顺之一。"],
     "internal_name": "SPADE_K",
-    "initial_unlocked": 1, "price_tier": 8, "weight": 2, "sprite_large": 116, "sprite_small": 117
+    "initial_unlocked": 1, "price_tier": 8, "weight": 2, "sprite_large": 124, "sprite_small": 125
   },
   "62": {
     "name": "黑桃 A",
     "desc": ["Miss 后的无敌时间 +50％。", "德州扑克・皇家同花顺之一。"],
     "internal_name": "SPADE_A",
-    "initial_unlocked": 1, "price_tier": 9, "weight": 2, "sprite_large": 116, "sprite_small": 117
+    "initial_unlocked": 1, "price_tier": 9, "weight": 2, "sprite_large": 126, "sprite_small": 127
   },
   "63": {
     "name": "强欲之壶",
