542263b44f79724349d508dced52f8c0ae761056
Renko6626 <renko6626@gmail.com>
2026-09-04 12:04:10 +0800

    card-expand: 同步构建产物（renkolab 97a05b8）
    DLL md5 5147a41c…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_test/th18/cards_dev.js
      thcrap/repos/Renko_1055/th18_card_expand_test/th18/st01bs.ecl
      mods/th18_card_expand.dll
      thcrap/repos/Renko_1055/th18_card_expand_test/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

--- thcrap/repos/Renko_1055/th18_card_expand_test/th18/cards_dev.js
+++ thcrap/repos/Renko_1055/th18_card_expand_test/th18/cards_dev.js
@@ -1,8 +1,5 @@
 {
-  "start_deck": [58],
-  "start_deck_force": true,
-  "start_owned": [59, 60, 61],
-  "start_money": 400,
+  "start_deck": [58, 59, 60, 61],
   "trace": true,
   "retail_weight": 6,
   "new_weight": 20
