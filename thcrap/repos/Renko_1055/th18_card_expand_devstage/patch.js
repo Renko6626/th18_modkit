7422565d5b8f9ce8c23fadf5fd6caedbe41a2357
Renko6626 <renko6626@gmail.com>
2026-09-04 20:31:32 +0800

    card-expand: 同步构建产物（renkolab b0c98e7）
    DLL md5 ae424389…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_devstage/patch.js
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st01.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st01bs.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st02.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st02bs.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st03.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st03bs.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st04.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st04bs.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st05.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st05bs.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st06.ecl
      thcrap/repos/Renko_1055/th18_card_expand_devstage/th18/st06bs.ecl
      thcrap/repos/Renko_1055/th18_card_expand_test/files.js
      thcrap/repos/Renko_1055/th18_card_expand_devstage/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

---
+++ thcrap/repos/Renko_1055/th18_card_expand_devstage/patch.js
@@ -0,0 +1,6 @@
+{
+  "id": "th18_card_expand_devstage",
+  "title": "(18) 开发用空壳关卡：st01–st06 道中全删、boss 血量 ÷100、boss 死时多掉 300 金。只想快速刷商店 / 试卡时勾；想打正常弹幕就别勾。不依赖别的 patch。",
+  "dependencies": [],
+  "update": false
+}
