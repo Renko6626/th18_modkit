7b0e78b987ea1b614e773c292f30dd0b1e3febd4
Renko6626 <renko6626@gmail.com>
2026-09-05 23:45:49 +0800

    card-expand: 同步构建产物（renkolab 0337488）
    DLL md5 e7f4fece…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/ability.anm
      mods/th18_card_expand.dll
      删除 thcrap/repos/Renko_1055/th18_card_expand_255/th18/pl00.sht
      删除 thcrap/repos/Renko_1055/th18_card_expand_255/th18/pl01.sht
      删除 thcrap/repos/Renko_1055/th18_card_expand_255/th18/pl02.sht
      删除 thcrap/repos/Renko_1055/th18_card_expand_255/th18/pl03.sht
      thcrap/repos/Renko_1055/th18_card_expand_255/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

--- thcrap/repos/Renko_1055/th18_card_expand_255/files.js
+++ thcrap/repos/Renko_1055/th18_card_expand_255/files.js
@@ -2,12 +2,8 @@
   "patch.js": 1388925521,
   "th18.v1.00a.js": 2650089801,
   "th18/abcard.anm": 1807917898,
-  "th18/ability.anm": 902000949,
+  "th18/ability.anm": 1905570093,
   "th18/cards.js": 2726177313,
-  "th18/pl00.sht": 2744854242,
-  "th18/pl01.sht": 4120164698,
-  "th18/pl02.sht": 2468020541,
-  "th18/pl03.sht": 1868824492,
   "th18/voice.js": 196876822,
   "th18/voice/ROYAL_RAGTIME.wav": 2245054909
 }
