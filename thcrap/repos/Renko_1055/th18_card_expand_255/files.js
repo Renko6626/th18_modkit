2537e73a8f4e95270f8e6c647ab758395496d639
Renko6626 <renko6626@gmail.com>
2026-09-05 19:27:46 +0800

    card-expand: 同步构建产物（renkolab 5a67339）
    DLL md5 cdda30a4…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/voice/ROYAL_FANFARE.wav
      thcrap/repos/Renko_1055/th18_card_expand_255/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

--- thcrap/repos/Renko_1055/th18_card_expand_255/files.js
+++ thcrap/repos/Renko_1055/th18_card_expand_255/files.js
@@ -5,5 +5,5 @@
   "th18/ability.anm": 1434088570,
   "th18/cards.js": 1069456578,
   "th18/voice.js": 461122510,
-  "th18/voice/ROYAL_FANFARE.wav": 1500559098
+  "th18/voice/ROYAL_FANFARE.wav": 1718531794
 }
