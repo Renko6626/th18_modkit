ff0c5803ba043e3f7cbc1a0591d107e397a2b373
Renko6626 <renko6626@gmail.com>
2026-09-05 19:09:47 +0800

    card-expand: 同步构建产物（renkolab c1ddcc0）
    DLL md5 85e62a42…
    改动：
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/voice.js
      mods/th18_card_expand.dll
      thcrap/repos/Renko_1055/th18_card_expand_255/th18/voice/TEST_VOICE.wav
      thcrap/repos/Renko_1055/th18_card_expand_255/files.js
    
    由 renkolab mods/th18.v1.00a/card-expand/native/release.py 生成。

--- thcrap/repos/Renko_1055/th18_card_expand_255/files.js
+++ thcrap/repos/Renko_1055/th18_card_expand_255/files.js
@@ -4,6 +4,6 @@
   "th18/abcard.anm": 2951222422,
   "th18/ability.anm": 1434088570,
   "th18/cards.js": 1069456578,
-  "th18/voice.js": 3265542873,
-  "th18/voice/TEST_VOICE.wav": 2493517302
+  "th18/voice.js": 205719396,
+  "th18/voice/TEST_VOICE.wav": 244241788
 }
--- thcrap/repos/Renko_1055/th18_card_expand_255/th18/voice.js
+++ thcrap/repos/Renko_1055/th18_card_expand_255/th18/voice.js
@@ -1,3 +1,3 @@
 {
-  "TEST_VOICE": { "wav": "TEST_VOICE", "id": 84, "volume": 100, "pan": 0 }
+  "TEST_VOICE": { "wav": "TEST_VOICE", "id": 84, "volume_db": 0, "priority": 100 }
 }
